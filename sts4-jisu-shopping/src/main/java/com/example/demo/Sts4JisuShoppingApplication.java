8eb94d44b662bc6be97e5ad6be83ec22231976e4
KIMKISEON12 <57428214+KIMKISEON12@users.noreply.github.com>
2021-03-01 18:49:31 +0900

    test

---
+++ sts4-jisu-shopping/src/main/java/com/example/demo/Sts4JisuShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sts4JisuShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sts4JisuShoppingApplication.class, args);
+	}
+
+}
